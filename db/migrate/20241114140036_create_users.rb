b63b0a6ab830abd977d7064f4b44ee2c08c8d7a0
Parag Verma <paragv1793@gmail.com>
2024-11-14 20:03:28 +0530

    added users table migration

---
+++ db/migrate/20241114140036_create_users.rb
@@ -0,0 +1,21 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def self.up
+    create_table :users do |t|
+      t.string :full_name, null: false
+      t.string :email, null: false
+      t.string :password
+      t.bigint :contact_number
+      t.string :profile_picture_url
+      t.date :date_of_birth
+      t.integer :status, default: 0
+      t.text :preferences
+      t.timestamps
+      t.datetime :deleted_at 
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+
+end
